03111f2a21af5761f113e90b26b8ac0b26624029
LINYUHAN1288 <1057000273@qq.com>
2020-07-26 09:17:33 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,14 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    "prefer-const": "off",
+    'eol-last': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'indent': 0
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/recommend'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/recommend',
+    component: () => import('components/recommend/recommend')
+  },
+  {
+    path: '/singer',
+    component: () => import('components/singer/singer'),
+    children: [
+      {
+        path: ':id',
+        component: () => import('components/singer-detail/singer-detail')
+      }
+    ]
   }
 ]
 
